d6045886e315d320b1732e1e8c3760b21033f8a8
mariaprimbas <primbasmaria@gmail.com>
2026-04-05 22:18:23 -0300

    Update metricas.py

--- src/metricas.py
+++ src/metricas.py
@@ -1,7 +1,20 @@
 # Funciones para cálculo de métricas
-
+ 
 #funcion 1: promedio señal
 def calcular_promedio_senal(datos_validos):
+   """
+    Calcular el promedio de los valores de la señal entre todos los participantes. 
+
+    Parameters
+    ----------
+    datos_validos : list
+        Lista de datos de participantes (cada participante es un diccionario)
+   
+    Returns
+    -------
+    float: numero que representa el promedio de la señal de todos los participantes, o 0 si no hay datos válidos.
+
+   """
   suma=0
   cantidad=0
 
@@ -18,6 +31,19 @@ def calcular_promedio_senal(datos_validos):
 
 #funcion maximo
 def calcular_maximo_senal(datos_validos):
+  """
+    Calcular el maximo de los valores de la señal entre todos los participantes. 
+
+    Parameters
+    ----------
+    datos_validos : list
+        Lista de datos de participantes (cada participante es un diccionario)
+   
+    Returns
+    -------
+    float: numero que representa el maximo de la señal de todos los participantes.
+
+   """
   maximo= None
 
    for participante in datos_validos:
@@ -28,6 +54,20 @@ def calcular_maximo_senal(datos_validos):
 
 #funcion minimo
 def calcular_minimo_senal(datos_validos):
+
+   """
+    Calcular el minimo de los valores de la señal entre todos los participantes. 
+
+    Parameters
+    ----------
+    datos_validos : list
+        Lista de datos de participantes (cada participante es un diccionario)
+   
+    Returns
+    -------
+    float: numero que representa el minimo de la señal de todos los participantes.
+
+   """
   minimo= None
 
    for participante in datos_validos:
