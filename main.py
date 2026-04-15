3077cc967ac36b8e7bdc8a7830d49147a855fa82
mariaprimbas <primbasmaria@gmail.com>
2026-04-15 14:33:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from src.validacion_datos import validar_registro
 from src.metricas import calcular_minimo_senal
 from src.metricas import calcular_maximo_senal
 from src.metricas import calcular_promedio_senal
+from src.procesamiento_datos import filtrar_datos
+from src.utils_ecg import detectar_picos
+from src.metricas import calcular_frecuencia_cardiaca
 
 datos= cargar_datos("datos/PulseLab_mock_data.csv")
 
@@ -15,9 +18,14 @@ for registro in datos:
     if validar_registro(registro):
         datos_validos.append(registro)
 
-promedio= calcular_promedio_senal(datos_validos)
-minimo= calcular_minimo_senal(datos_validos)
-maximo= calcular_maximo_senal(datos_validos)
+id_participante = input("Ingrese un id: ")
+
+datos_filtrados= filtrar_datos(datos_validos, id_participante)
+promedio= calcular_promedio_senal(datos_filtrados)
+minimo= calcular_minimo_senal(datos_filtrados)
+maximo= calcular_maximo_senal(datos_filtrados)
+picos= detectar_picos()
+fc= calcular_frecuencia_cardiaca(picos)
 
 resultado= (promedio, minimo, maximo)
 
