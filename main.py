d7603cad4da61d3d6241813216ea13795c6249c2
mariaprimbas <primbasmaria@gmail.com>
2026-04-15 14:34:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.metricas import calcular_minimo_senal
 from src.metricas import calcular_maximo_senal
 from src.metricas import calcular_promedio_senal
 from src.procesamiento_datos import filtrar_datos
-from src.utils_ecg import detectar_picos
+from src.utils_ecg import detectar_picos_qrs
 from src.metricas import calcular_frecuencia_cardiaca
 
 datos= cargar_datos("datos/PulseLab_mock_data.csv")
@@ -24,7 +24,7 @@ datos_filtrados= filtrar_datos(datos_validos, id_participante)
 promedio= calcular_promedio_senal(datos_filtrados)
 minimo= calcular_minimo_senal(datos_filtrados)
 maximo= calcular_maximo_senal(datos_filtrados)
-picos= detectar_picos()
+picos= detectar_picos_qrs()
 fc= calcular_frecuencia_cardiaca(picos)
 
 resultado= (promedio, minimo, maximo)
