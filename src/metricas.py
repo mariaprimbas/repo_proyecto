91fc6b2396e4698f07b56de05b5858ec9cb3a70b
mariaprimbas <primbasmaria@gmail.com>
2026-04-16 14:29:20 -0300

    Update metricas.py

--- src/metricas.py
+++ src/metricas.py
@@ -18,19 +18,20 @@ def calcular_promedio_senal(datos_filtrados):
    if len(datos_filtrados) == 0:
         raise ValueError("No hay datos para calcular el promedio") 
 
-  lista_prom_elemento= []
-  for elemento in datos_filtrados:
-     suma=0
-     cantidad=0
+   lista_prom_elemento= []
+   for elemento in datos_filtrados:
+      suma=0
+      cantidad=0
 
-     for senal in datos_filtrados["valor"]:
-        suma+=senal
-        cantidad+=1
-     promedio=suma/cantidad
-     if cantidad == 0:
-        raise ValueError("No hay valores de señal")
-     else:
-      lista_prom_elemento.append(promedio)
+      for senal in datos_filtrados["valor"]:
+         suma+=senal
+         cantidad+=1
+      promedio=suma/cantidad
+    
+      if cantidad == 0:
+         raise ValueError("No hay valores de señal")
+      else:
+         lista_prom_elemento.append(promedio)
   
    promedio_todos=sum(lista_prom_elemento)/len(lista_prom_elemento)
 
