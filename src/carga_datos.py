277713d4a56d05a61c91d9f8ed6c5756ea20c140
glorialderetec <galderetecornejo@udesa.edu.ar>
2026-05-21 14:22:13 -0300

    Update carga_datos.py

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -1,6 +1,6 @@
 def cargar_datos(ruta_archivo):
     '''
-    Leer un archivo y transformar su contenido en una lista de diccionarios.
+    Leer un archivo y transformar su contenido en un dataframe
 
     Parameters
     ----------
@@ -12,63 +12,6 @@ def cargar_datos(ruta_archivo):
     list
         Lista con los registros agrupados por participante.
     '''
-
-    datos = []
-    participantes = {}
-
-    try:
-        archivo = open(ruta_archivo, "r", encoding="utf-8")
-        lineas = archivo.readlines()
-        archivo.close()
-    except FileNotFoundError:
-        print("ERROR CRITICO: no se encontró el archivo.- Ubicacion: carga_datos")
-        return []
-
-    for linea in lineas:
-        if linea.strip() == "":
-            continue
-
-        partes = parsear_linea(linea)
-
-        if len(partes) != 6:
-            print("ERROR: línea mal formada:", linea.strip())
-            continue
-        try:
-            id_participante = int(partes[0])
-            tiempo = float(partes[1])
-            valor = float(partes[2])
-        except ValueError:
-            print("ERROR: tipos de datos inválidos en la línea:",linea.strip())
-            continue
-
-        fase = partes[3]
-        condicion_experimental = partes[4]
-        hit = partes[5]
-
-        if id_participante not in participantes:
-            participantes[id_participante] = {
-                "id_participante": id_participante,
-                "tiempo": [],
-                "valor": [],
-                "fase": [],
-                "condicion_experimental": [],
-                "hit": []
-            }
-
-        participantes[id_participante]["tiempo"].append(tiempo)
-        participantes[id_participante]["valor"].append(valor)
-        participantes[id_participante]["fase"].append(fase)
-        participantes[id_participante]["condicion_experimental"].append(condicion_experimental)
-        participantes[id_participante]["hit"].append(hit)
-
-    for participante in participantes:
-        datos.append(participantes[participante])
-
-    return datos
-
-
-def parsear_linea(linea):
-    '''
-    Transformar una línea del archivo en una lista de valores.
-    '''
-    return linea.strip().split(",")
+    import pandas as pd
+    df = pd.read_csv("ruta_archivo.csv")
+    df_indice = df.set_index("id_inscripcion")
