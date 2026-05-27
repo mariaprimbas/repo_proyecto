cc472487fffa73901d1220441fe50ee640162f79
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-05-27 15:18:17 -0300

    Update procesamiento_datos.py

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,13 +1,13 @@
 
 # Funciones para procesamiento de datos
 
-def filtrar_datos(datos, id_participante):
+def filtrar_datos(df, id_participante):
    """
     Filtrar los datos de un Dataframe por participante a partir de su id. 
 
     Parameters
     ----------
-    datos : DataFrame
+    df : DataFrame
       Dataframe con datos de los participantes 
     id_participante: int
       id ingresado que define los datos del participante que quiere filtrar 
@@ -17,39 +17,19 @@ def filtrar_datos(datos, id_participante):
     Dataframe: datos filtrados 
 
    """
-   datos_filtrados=[]
+   if id_participante == "todos": 
+      return datos 
    while True:
       id_participante= input("Ingrese ID del participante o -todos- para analizar todos los participantes: ")
      
       if id_participante=="todos":
-         datos_filtrados = datos
-         
-      elif id_participante.isdigit():
-            
-      
-            id_participante_int = int(id_participante)
-            if id_participante_int <= 0: 
-               raise ValueError("ERROR CRITICO: id invalido- Ubicacion: filtrar_datos in procesamiento_datos") 
-               
-            encontrado = False 
-         
-            for participante in datos: 
-               id_f = participante["id_participante"]
-               if id_f == id_participante_int: 
-                  datos_filtrados.append(participante)
-                  encontrado = True
-                  break
-               
-            if encontrado == False: 
-               print("ID no encontrado. Intente nuevamente")
-                 
-      else:
-         raise ValueError("ERROR CRITICO: Dato ingresado no valido - Ubicacion: filtrar_datos in procesamiento_datos")
-         continue
-      pregunta = input("Desea seguir buscando un participante? ")
-     
-      if pregunta.lower() == "no": 
-         break 
-         
-   return datos_filtrados
+         datos_filtrados = df
 
+      elif: 
+         datos_filtrados = df[df["id_participante"] == id_participante]
+      else: 
+         print("Id no encontrado")
+
+      return datos_filtrados 
+         
+   
